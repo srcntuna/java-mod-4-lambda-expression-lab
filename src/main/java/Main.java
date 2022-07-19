205f5e277b71de2c6435245b63e27bd23808ecb6
Sercan Tuna <ssercantuna@hotmail.com>
2022-07-19 13:30:42 -0400

    done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,26 @@ import java.util.function.IntUnaryOperator;
 
 public class Main {
 	// write the nextOddNum here
+
+    interface NextOddNum{
+        Integer applyAsInt(Integer num);
+    }
     
     public static void main(String[] args) {
+
+        NextOddNum nextOddNum = (num -> {
+            if(num%2 == 0){
+                return num +1 ;
+            }
+            return num+2;
+        });
+
         int num = 5;
         int num2 = 10;
 
         System.out.println(nextOddNum.applyAsInt(num)); // 7
         System.out.println(nextOddNum.applyAsInt(num2)); // 11
     }
+
+
 }
